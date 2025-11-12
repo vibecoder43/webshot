2d30d620a095d7a381d0479d23a9f3d99b733af8
vibecoder43 <55918427+vibecoder43@users.noreply.github.com>
2025-11-13 02:23:01 +0400

    feat(db): update schema and sql
    handle links instead of URLs

--- sql/schema/webshot_db_schema.sql
+++ sql/schema/webshot_db_schema.sql
@@ -1,7 +1,7 @@
 create table webshot (
     id uuid primary key default gen_random_uuid(),
     created_at timestamptz not null default now(),
-    url text collate "C" not null
+    link text collate "C" not null
 );
 
-create index items_url_prefix_idx on webshot(url, created_at desc, id);
+create index items_url_prefix_idx on webshot(link, created_at desc, id);
--- src/include/sql.hpp
+++ src/include/sql.hpp
@@ -4,7 +4,7 @@
 namespace sql {
 
 inline constexpr std::string_view kInsertWebshot = R"~(
-insert into webshot(id, created_at, url)
+insert into webshot(id, created_at, link)
 values(default, default, $1)
 returning id
 )~";
@@ -13,8 +13,8 @@ inline constexpr std::string_view kSelectWebshot = R"~(
 select id from webshot where id = $1
 )~";
 
-inline constexpr std::string_view kSelectWebshotByUrl = R"~(
-select id, created_at from webshot where url = $1 order by created_at desc limit $2
+inline constexpr std::string_view kSelectWebshotByLink = R"~(
+select id, created_at from webshot where link = $1 order by created_at desc limit $2
 )~";
 
 }; // namespace sql
